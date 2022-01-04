1cc1c42a7725456cbf8035d4894bfd534c6d603c
aphashemian <amir.p.hashemian@gmail.com>
2022-01-03 18:38:36 -0600

    Fixed presentation and set up operation foundation.

--- index.js
+++ index.js
@@ -2,14 +2,42 @@
 let current = "";
 let valueCurrent = 0;
 let currentEl = document.getElementById("display-el");
+let state = "NONE";
 
 function pressNumber(key)
 {
   current+= key;
   currentEl.textContent += key;
   valueCurrent = parseInt(current);
+  console.log(key);
 }
 
+function pressOperation(key)
+{
+  switch (key)
+  {
+    case "+": state = "ADD";
+              break;
+    case "-": state = "SUB";
+              break;
+    case "*": state = "MUL";
+              break;
+    case "/": state = "DIV";
+              break;
+  }
+
+}
+
+function solve()
+{
+  switch(state)
+  {
+    case "ADD":
+    case "SUB":
+    case "MUL":
+    case "DIV": 
+  }
+}
 function save()
 {
   let countStr = count + " - ";
